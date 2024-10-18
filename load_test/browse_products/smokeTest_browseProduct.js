e15f11c02e70414b71e205aaecc023886da5a0c0
cikada.sayyidil <cikadasayyidilakbar@gmail.com>
2024-10-19 00:14:54 +0700

    fix indentation

--- load_test/browse_products/smokeTest_browseProduct.js
+++ load_test/browse_products/smokeTest_browseProduct.js
@@ -1,32 +1,32 @@
 import http from 'k6/http'
-import { check, group} from 'k6';
+import { check, group } from 'k6';
 
 
 export const options = {
-    vus: 20,
-    duration: '15s',
-    thresholds: {
-        'http_reqs{group:::smoke_test}': ['rate>=5'],               //Set thresholds metrics for Throughput
-        'http_req_duration{group:::smoke_test}': ['p(95)<400'],     //Set thresholds metrics for Response Time
-        'http_req_failed{group:::smoke_test}': ['rate<0.05'],       //Set thresholds metrics for Error Rate
-      },
+  vus: 20,
+  duration: '15s',
+  thresholds: {
+    'http_reqs{group:::smokeTest}': ['rate>=5'],               //Set thresholds metrics for Throughput
+    'http_req_duration{group:::smokeTest}': ['p(95)<400'],     //Set thresholds metrics for Response Time
+    'http_req_failed{group:::smokeTest}': ['rate<0.05'],       //Set thresholds metrics for Error Rate
+  },
 }
 
 //Function to create random number
 function randomize(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export default function get_items_category() {
+export default function getItemsCategory() {
 
-    const category = ["notebook", "phone", "monitor"];   //Create Array of Categories
-    let num = randomize(0, 2); //Create random number for array categories
+  const category = ["notebook", "phone", "monitor"];   //Create Array of Categories
+  let num = randomize(0, 2); //Create random number for array categories
 
-    group('smoke_test', () => {
-        let bodyRequest = { cat: category[num] };
-        let response = http.post('https://api.demoblaze.com/bycat', JSON.stringify(bodyRequest), {
-            headers: { 'Content-Type': 'application/json' },
-        });
-        check(response, { 'status is 200': (r) => r.status === 200 });
-    })
+  group('smokeTest', () => {
+    let bodyRequest = { cat: category[num] };
+    let response = http.post('https://api.demoblaze.com/bycat', JSON.stringify(bodyRequest), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+    check(response, { 'status is 200': (r) => r.status === 200 });
+  })
 }
--- load_test/browse_products/stressTest_browseProduct.js
+++ load_test/browse_products/stressTest_browseProduct.js
@@ -1,40 +1,40 @@
 import http from 'k6/http'
-import { check, group} from 'k6';
+import { check, group } from 'k6';
 
 export const options = {
     // vus: 1500,
-    stages: [
-        { duration: '20s', target:300 },
-        { duration: '40s', target:300 },
-        { duration: '20s', target:700 },
-        { duration: '40s', target:700 },
-        { duration: '20s', target:1200 },
-        { duration: '40s', target:1200 },
-        { duration: '40s', target:0 }
-    ],
-    thresholds: {
-        'http_reqs{group:::stress_test}': ['rate>=5'],               //Set thresholds metrics for Throughput
-        'http_req_duration{group:::stress_test}': ['p(95)<400'],     //Set thresholds metrics for Response Time
-        'http_req_failed{group:::stress_test}': ['rate<0.05'],       //Set thresholds metrics for Error Rate
-      }
+  stages: [
+    { duration: '20s', target: 300 },
+    { duration: '40s', target: 300 },
+    { duration: '20s', target: 700 },
+    { duration: '40s', target: 700 },
+    { duration: '20s', target: 1200 },
+    { duration: '40s', target: 1200 },
+    { duration: '40s', target: 0 }
+  ],
+  thresholds: {
+    'http_reqs{group:::stressTest}': ['rate>=5'],               //Set thresholds metrics for Throughput
+    'http_req_duration{group:::stressTest}': ['p(95)<400'],     //Set thresholds metrics for Response Time
+    'http_req_failed{group:::stressTest}': ['rate<0.05'],       //Set thresholds metrics for Error Rate
+    }
 }
 
 
 //Function to create random number
 function randomize(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export default function get_items_category() {
+export default function getItemsCategory() {
 
-    const category = ["notebook", "phone", "monitor"];   //Create Array of Categories
-    let num = randomize(0, 2); //Create random number for array categories
+  const category = ["notebook", "phone", "monitor"];   //Create Array of Categories
+  let num = randomize(0, 2); //Create random number for array categories
 
-    group('stress_test', () => {
-        let bodyRequest = { cat: category[num] };
-        let response = http.post('https://api.demoblaze.com/bycat', JSON.stringify(bodyRequest), {
-            headers: { 'Content-Type': 'application/json' },
-        });
-        check(response, { 'status is 200': (r) => r.status === 200 });
-    })
+  group('stressTest', () => {
+    let bodyRequest = { cat: category[num] };
+    let response = http.post('https://api.demoblaze.com/bycat', JSON.stringify(bodyRequest), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+    check(response, { 'status is 200': (r) => r.status === 200 });
+  })
 }
